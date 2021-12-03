57c6117a256015340fe084b8d4eb9b68d88f1f4d
Rouwaida <engrouwaida@gmail.com>
2021-12-04 00:36:11 +0200

    adjusting the code to read env apikey at server

--- src/client/js/handleSubmit.js
+++ src/client/js/handleSubmit.js
@@ -1,15 +1,15 @@
 
 function handleSubmit(event) {
-    event.preventDefault()
+  event.preventDefault()
 
-    // check what text was put into the form field
-    let formText = document.getElementById('myURL').value
-    // let formText = "https://en.wikipedia.org/wiki/Margaret_Hamilton_(software_engineer)"
-    console.log("The url typed is",formText)
-    const urlCheckerResult = MyClient.checkForRightURL(formText)
-    console.log("URL Checker Result is:",urlCheckerResult)
-    console.log("Rou API key is",process.env.myAPIKey);
-    
+  // check what text was put into the form field
+  let formText = document.getElementById('myURL').value
+  // let formText = "https://en.wikipedia.org/wiki/Margaret_Hamilton_(software_engineer)"
+  console.log("The url typed is",formText)
+  const urlCheckerResult = MyClient.checkForRightURL(formText)
+  console.log("URL Checker Result is:",urlCheckerResult)
+  
+
 //********************************  START  ****************************************** */
 
 //Adding event listener on click for the button and calling prepareArticleData function
@@ -18,131 +18,102 @@ document.getElementById('btnSubmit').addEventListener('click', prepareArticleDat
 }//End of handleSubmit
 
 /**
- * @description get the entered URL from input, 
- *              check if the entered URL is right and valid URL or not
- *              calling the fetch API and chained promises
+* @description get the entered URL from input, 
+*              check if the entered URL is right and valid URL or not
+*              calling the fetch API and chained promises
 */
 function prepareArticleData(){
+  try{
 const inputUserURL =  document.getElementById('myURL').value
 const isRightURL = MyClient.checkForRightURL(inputUserURL)
-const myAPIKeyToSend = '?key=35f1e8cb2902d9b55dfd542e119b1ad5'
-const meaningCloudUrl = 'https://api.meaningcloud.com/sentiment-2.1'
+
 if (isRightURL===false)
 {
-    alert('Kindly enter a valid URL!');
+  alert('Kindly enter a valid URL!');
 }
 else
 {
-  //chaining promises here 3 functions call
 
-    const urlToSend = "&url=" + inputUserURL
-    console.log("meaningCloudUrl",meaningCloudUrl) //for test
-    console.log("urlToSend",urlToSend)  //for test
-    console.log("APIPortionToSend",myAPIKeyToSend)  //for test
 
-    let myReturnedArticleData=getArticleData(meaningCloudUrl,urlToSend,myAPIKeyToSend);
-    myReturnedArticleData
-    .then(function(data){      //Rou post new call
-        // postData('/addData',{
-      postData('http://localhost:8081/addData',{
+  const urlToSend = inputUserURL
+ 
+  console.log("FROM CLIENT:urlToSend",urlToSend)  //for test
+ 
 
-        model: data.model,
-        score_tag: data.score_tag,
-        agreement:data.agreement,
-        subjectivity:data.subjectivity,
-        confidence:data.confidence,
-        irony:data.irony
-                 
-    });
-      })// end of .then
-    .then(()=>updateMyUI())
-   
-    }//end of else
+  const myReturnedArticleData=getArticleData(urlToSend);
+  
+ 
+  }//end of else
 
+}
+catch(error){
+  console.log("NEW error in prepareArticleData", error);
+}
 }  // rou end of prepareArticleData
 
 //Rou definition of updateMyUI
 
 /**
- * @description asynchronous function used to fech API using API key, Article URL entered by user
- * @param {string} mCUrl - The (meaning cloud) url
- * @param {string} iUURL - The (input user) url portion
- * @param {string} api - The api key received while making credentials on meaning cloud
- * @returns {string} data - the response of the meaning could api after transforming it to javascript format
+* @description asynchronous function used to fech API using Article URL entered by user and update the user interface
+* @param {string} iUURL - The (input user) url portion
+* @returns {string} data - the response of the meaning could api after transforming it to javascript format
 */
-const getArticleData = async (mCUrl, iUURL, api)=>{
-    
-    const res = await fetch(mCUrl+api+iUURL+'&lang=en');
-    try {
+const getArticleData = async (iUURL)=>{
+  
+  
+  try {
+    const res = await postData("http://localhost:8081/addData",{iUURL});
 
-        //convert returned data from json format to javascript format
-        const data = await res.json();
 
-        console.log(data);  // for test
-           
-        return data; //the returned data
+    document.getElementById('model').innerHTML='Model: ' + res.model;
+    document.getElementById('score_tag').innerHTML='Score Tag: ' + res.score_tag;
+    document.getElementById('agreement').innerHTML='Agreement: ' + res.agreement;
+  
+    document.getElementById('subjectivity').innerHTML='Subjectivity: ' + res.subjectivity;
+    document.getElementById('confidence').innerHTML='Confidence: ' + res.confidence;
+    document.getElementById('irony').innerHTML='Irony: ' + res.irony;
 
-      } catch(error){
-        console.log("error in getArticleData", error);
-        
-      }
-    }//end of getArticleData
+     
+    } catch(error){
+      console.log("error in getArticleData", error);
+      
+    }
+  }//end of getArticleData
 
 //Rou definition of post request
 //
 
 /**
- * @description asynchronous function used to post received article api to the server endpoint
- * @param {string} url - The url of the post function
- * @param {function} data - The callback function that define the post response
+* @description asynchronous function used to post received article api to the server endpoint
+* @param {string} url - The url of the post function
+* @param {function} data - The callback function that define the post response
 */
 
-  const postData = async(url, data={})=>{
+const postData = async(url, data={})=>{
 console.log("the url is:" , url)
 try {
-  const response = await fetch(url, {
-    "method": 'POST',
-    "credentials": 'same-origin',
-    headers: {
-        "Content-Type": 'application/json'
-    },
-    body: JSON.stringify(data) 
-  });
-  // try {
-  
-    return;
-  }catch(error) {
-    console.log("Post error", error);
-  }
+const response = await fetch(url, {
+  "method": 'POST',
+  "credentials": 'same-origin',
+        "mode":'cors',
+  headers: {
+      "Content-Type": 'application/json'
+  },
+  body: JSON.stringify(data) 
+});
+// try {
+
+  // return;
+  return await response.json();
+
+}catch(error) {
+  console.log("Post error", error);
+}
 
 }//end of post function
 
-//Rou definition of updateMyUI
-
-/**
- * @description asynchronous function used to update user interface by the data registered in the endpoint defined at server/index.js (projectData)
- * @param {function} anynomous - The callback function that wait for get request to read endpoint data and update the interface from it
-*/
-const updateMyUI = async ()=>{
-  const myRequest = await fetch('http://localhost:8081/all')
-  try{
-    const myReturnedData = await myRequest.json();
-   
-    document.getElementById('model').innerHTML='Model: ' + myReturnedData.model;
-    document.getElementById('score_tag').innerHTML='Score Tag: ' + myReturnedData.score_tag;
-    document.getElementById('agreement').innerHTML='Agreement: ' + myReturnedData.agreement;
-
-    document.getElementById('subjectivity').innerHTML='Subjectivity: ' + myReturnedData.subjectivity;
-    document.getElementById('confidence').innerHTML='Confidence: ' + myReturnedData.confidence;
-    document.getElementById('irony').innerHTML='Irony: ' + myReturnedData.irony;
-
-  }
-  catch(err){
-    console.log('Error in updateMyUI ' , err);
-  }
-}
 
 //********************************    END    ************************************************** */
- 
+
 export { handleSubmit }
 
--- src/server/index.js
+++ src/server/index.js
@@ -1,13 +1,11 @@
-
 //settings and initializations
 //1- setup the enviromental variables
-const dotenv = require('dotenv')
-dotenv.config({
-    path: `${__dirname}/src/.env`
-  })
+const dotenv = require('dotenv');
+dotenv.config()
 
 // Setup an empty JS object to act as endpoint for all routes in my project
 projectData = {}
+
 
 var path = require('path')
 
@@ -15,7 +13,6 @@ var path = require('path')
 const express = require('express')
 
 const app = express()
-
 
 console.log(__dirname) //for test
 console.log("Rourou API key is",process.env.myAPIKey) //for test
@@ -29,7 +26,8 @@ app.use(bodyParser.json())
 // Cors for cross origin allowance
 const cors = require('cors')
 app.use(cors())
-
+//axios
+const axios = require ('axios');
 // Initialize the main project folder (dist)
 app.use(express.static('dist'))
 
@@ -52,16 +50,16 @@ app.listen(8081, function () {
 
 //get route
 
-/**
- * @description defining get route and send the endpoint (projectData) as server response
- * @param {string} /all - The url of the get route
- * @param {function} anynomous - The function with request and response parameters
-*/
-app.get('/all',function(req,res){
+// /**
+//  * @description defining get route and send the endpoint (projectData) as server response
+//  * @param {string} /all - The url of the get route
+//  * @param {function} anynomous - The function with request and response parameters
+// */
+// app.get('/all',function(req,res){
     
-   console.log('model ' + projectData.model + ' score_tag:' + projectData.score_tag + ' agreement:' + projectData.agreement)
-    res.send(projectData)
-  })
+//    console.log('model ' + projectData.model + ' score_tag:' + projectData.score_tag + ' agreement:' + projectData.agreement)
+//     res.send(projectData)
+//   })
   
   //post route
   
@@ -69,31 +67,42 @@ app.get('/all',function(req,res){
    * @description defining post route and send the endpoint (projectData) as server response
    *              also it writes in the console - terminal - the endpoint data for test purposes
    * @param {string} /addData - The url of the get route
-   * @param {function} addArticleData - The function with request and response parameters that fill the endpoint with received data
-  */
-  app.post('/addData', addArticleData)
-  
-  /**
-   * @description fills the endpoint (projectData) with the received data (model, score_tag, agreement, subjectivity,confidence,irony)
-   *              sends the projectData endpoint to the client
-   * @param {object} req - the request
-   * @param {object} res - The response
-  */
-  function addArticleData(req, res){
-   
-    console.log('from server: request :' + req.body)
-  
-    projectData.model=req.body.model;
-    projectData.score_tag=req.body.score_tag;
-    projectData.agreement=req.body.agreement;
 
-    projectData.subjectivity=req.body.subjectivity;
-    projectData.confidence=req.body.confidence;
-    projectData.irony=req.body.irony;
+  */
+
+app.post('/addData', async (req, res) => {
+   
+  const myAPIToSend = process.env.myAPIKey
+  const meaningCloudUrl = 'https://api.meaningcloud.com/sentiment-2.1'
+  const urlToSend = `${meaningCloudUrl}?key=${myAPIToSend}&url=${req.body.iUURL}&lang=en`
+  console.log("NEW FROM SERVER the urlToSend is:",urlToSend)
+  // https://api.meaningcloud.com/sentiment-2.1?key=35f1e8cb2902d9b55dfd542e119b1ad5&url=https://en.wikipedia.org/wiki/Margaret_Hamilton_(software_engineer)&lang=en
  
+    try {
+
+
+    const response = await axios(urlToSend);
+     
+    projectData.model=response.data.model;
+    projectData.score_tag=response.data.score_tag;
+    projectData.agreement=response.data.agreement;
+
+    projectData.subjectivity=response.data.subjectivity;
+    projectData.confidence=response.data.confidence;
+    projectData.irony=response.data.irony;
+
     res.send(projectData)
-    console.log('This is server - projectData' + projectData)//for test s
-    
-  } 
   
+
+      } catch(error){
+        console.log("error in getAPIData", error);
+        
+      }
+    // }//end of getArticleData
+
+  
+} // end getAPIData
+
+)
+
 //********************************   Rou END SERVER ******************************************* */
